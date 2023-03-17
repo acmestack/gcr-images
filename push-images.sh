858afabcb1fc8c1a7c138820eafff20607488402
qicz <qiczzhu@gmail.com>
2023-03-17 13:44:43 +0800

    add skip logic
    Signed-off-by: qicz <qiczzhu@gmail.com>

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 docker buildx ls
 docker buildx create --name imagesbuilder --use
+current_date=$(date '+%Y-%m-%d')
 
 cd images
 pwd=`pwd`
@@ -10,6 +11,15 @@ do
     if [ -d $image ]
     then
         cd `echo $image`
+        echo "image "$image
+        last_time=$(stat -c %Y name)
+        formart_last_date=$(date '+%Y-%m-%d' -d @$last_time)
+        echo "last modify date "$formart_last_date
+        if [ "$formart_last_date" != "$current_date" ]; then
+            echo "$image pushed, skip..."
+            cd ..
+            continue
+        fi
         echo "building $image"
         docker buildx build --push --platform linux/amd64,linux/arm64 -t acmestack/`cat name`:`cat tag` .
         cd ..
