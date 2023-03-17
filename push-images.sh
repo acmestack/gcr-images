269719ff073f59bfc5a4d764261879db126ab6ce
qicz <qiczzhu@gmail.com>
2023-03-17 14:24:03 +0800

    update push sh

--- push-images.sh
+++ push-images.sh
@@ -10,12 +10,21 @@ for image in $images
 do
     if [ -d $image ]
     then
+        echo "-----"
         cd `echo $image`
-        echo "image "$image
-        last_time=$(stat -c %Y name)
-        formart_last_date=$(date '+%Y-%m-%d' -d @$last_time)
-        echo "last modify date "$formart_last_date
-        if [ "$formart_last_date" != "$current_date" ]; then
+        echo "processing image "$image
+        echo "🦁"
+        skip_image="true"
+        for item in `ls $dir`
+        do
+            formart_last_date=$(git log -1 --format="%at" $item | xargs -I{} date -d @{} +%Y-%m-%d)
+            echo "$image/$item last modify date $formart_last_date"
+            if [ "$formart_last_date" = "$current_date" ]; then
+                skip_image="false"
+                break
+            fi                
+        done
+        if [ "$skip_image" = "true" ]; then
             echo "$image pushed, skip..."
             cd ..
             continue
